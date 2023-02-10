dc50258a4795f76b4a66495ced001fdc5ff2b73a
archtaqi <taqi.arch@gmail.com>
2023-02-11 01:08:23 +0500

    init project

---
+++ src/config/config.js
@@ -0,0 +1,33 @@
+
+
+/* Pinata Keys ********************************
+
+API Key: 
+API Secret: 
+JWT: 
+
+Pinata Gateway:
+
+JWT: 
+
+*/
+
+export const apike = "";
+export const apisecret = "";
+export const gatewayjwt = "";
+export const ipfsgateway = "";
+
+export const readHeader = {
+	"Content-Type": "application/json",
+}
+
+export const getHeader = {
+	headers: {
+		'Content-Type': 'application/json',
+		pinata_api_key: apikey,
+		pinata_api_key_secret: apisecret,
+	}
+}
+
+
+
